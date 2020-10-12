0cb7580ce7ed7b590a9fe8c29c92753083c6c195
otman el hajjaji <otman.pilote.96@gmail.com>
2020-10-12 23:48:49 +0100

    add mainapp class

--- code/index.py
+++ code/index.py
@@ -8,12 +8,12 @@ from os import path
 import torch
 import numpy as np
 
-FORM_CLASS,_= loadUiType(path.join(path.dirname(__file__),"ui/main.ui"))
+FORM_CLASS,_= loadUiType(r"C:/Users/Otman/Desktop/PLC/qt_app_mts/ui/login.ui")
 
 def test1():
 	print('it works')
 
-class MainApp(QMainWindow,FORMCLASS):
+class MainApp(QMainWindow,FORM_CLASS):
     """docstring for MainApp"""
     def init(self, arg=None):
         super(MainApp, self).init(arg)
@@ -29,5 +29,6 @@ def main():
 def ui():
 	print('test')
 
-if name == 'main':
-    main()
+if __name__ == '__main__':
+    print(path.join(path.dirname(__file__),"ui\main.ui"))
+    main()    
