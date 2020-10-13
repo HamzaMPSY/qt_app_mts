f42b2b310a2ecdd83de869263b035dd199f9a68b
Hamza EL MAKRINI <elmakrinihamza1@gmail.com>
2020-10-13 17:27:57 +0100

    users table

--- code/index.py
+++ code/index.py
@@ -51,7 +51,6 @@ class MainApp(QMainWindow,LOGIN_UI):
                 QMessageBox.warning(self,"Error","Login or Password incorrect!")
         
 
-
 class Admin(QMainWindow,ADMIN_UI):
     switchWindow = pyqtSignal()
 
@@ -85,10 +84,20 @@ class Admin(QMainWindow,ADMIN_UI):
         users = pd.read_csv('../files/users.csv',delimiter =";")
         model = pandasModel(users)
         self.tableView.setModel(model)
-        #self.tableView.show()
+        self.tableView.horizontalHeader().setStretchLastSection(True) 
+        self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        self.tableView.clicked.connect(self.modifyUser)
+
+    def modifyUser(self,item):
+        row = item.row()
+        res = pd.read_csv('../files/users.csv',delimiter=";").iloc[row,:]
+        username = res['username']
+        password = res['password']
+        isadmin = res['isadmin']
+        editDialog = EditDialog(username,password,isadmin,row)
+        editDialog.exec_()
+        self.users()
   
-
-
 class Controller:
 
     def __init__(self):
@@ -117,7 +126,7 @@ class pandasModel(QAbstractTableModel):
     def rowCount(self, parent=None):
         return self._data.shape[0]
 
-    def columnCount(self, parnet=None):
+    def columnCount(self, parent=None):
         return self._data.shape[1]
 
     def data(self, index, role=Qt.DisplayRole):
@@ -130,6 +139,49 @@ class pandasModel(QAbstractTableModel):
         if orientation == Qt.Horizontal and role == Qt.DisplayRole:
             return self._data.columns[col]
         return None
+
+class EditDialog(QDialog):
+    def __init__(self, username,password,isadmin,row, parent=None):
+        super().__init__(parent)
+
+        self.username = QLineEdit(self)
+        self.password = QLineEdit(self)
+        self.isadmin = QLineEdit(self)
+        self.row=row
+        self.username.setText(username)
+        self.password.setText(password)
+        self.isadmin.setText(str(isadmin))
+        
+        buttonBox = QDialogButtonBox(Qt.Horizontal)
+        buttonBox.addButton("Save", QDialogButtonBox.AcceptRole)
+        buttonBox.addButton("Delete", QDialogButtonBox.HelpRole)
+        buttonBox.addButton("Cancel", QDialogButtonBox.RejectRole)
+
+        layout = QFormLayout(self)
+        layout.addRow("Username :", self.username)
+        layout.addRow("Password :", self.password)
+        layout.addRow("Is Admin :", self.isadmin)
+        layout.addWidget(buttonBox)
+
+        buttonBox.accepted.connect(self.accept)
+        buttonBox.rejected.connect(self.reject)
+        # buttonBox.helpRequested.connect(self.delete)
+
+        def accept(self):
+            df = pd.read_csv('../files/users.csv',delimiter=";")
+            df[self.row,'username'] = self.username.text()
+            df[self.row,'password'] = self.password.text()
+            df[self.row,'isadmin'] = int(self.isadmin.text())
+            df.to_csv('../files/users.csv',index=False)
+
+        def delete(self):
+            pass
+
+        def reject(self):
+            pass
+
+    def getInputs(self):
+        return (self.first.text(), self.second.text())
 
 def main():
     app = QApplication(sys.argv)
