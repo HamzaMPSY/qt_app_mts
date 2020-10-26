4e31b313127cf8f197f41ef40a4d157f19f58a9f
Hamza EL MAKRINI <elmakrinihamza1@gmail.com>
2020-10-26 12:53:15 +0100

    pie

--- code/statistics.py
+++ code/statistics.py
@@ -5,6 +5,17 @@ import pandas as pd
 def plotNumberOfScans(column,target):
 	hist = pd.read_csv('../files/history.csv',parse_dates =['date'])
 	res = hist[hist[column] == target]
-	dim = (5,3)
-	sns.catplot(x=column, hue="response", kind="count" ,data=res,height = dim[1],aspect = dim[0]/dim[1])
+	data = res.groupby("response")["reference"].count()
+	pie, ax = plt.subplots(figsize=[5,3])
+	labels = list(data.keys())
+	for i in range(len(labels)):
+		labels[i] = str(data[labels[i]].item()) + ', '+ labels[i]
+	plt.pie(x=data, autopct="%.1f%%", explode=[0.05]*len(labels), labels=labels, pctdistance=0.5)
+	if column == "username":
+		plt.title("Number of successful/failed scans by "+target, fontsize=14)
+	else:
+		plt.title("Number of successful/failed scans of "+target, fontsize=14)
 	plt.savefig('../assets/'+column+'.png')
+
+if __name__ == '__main__':
+	plotNumberOfScans('reference','ref1')
