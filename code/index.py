4309fa5af2384bb60b02165429e502d1ebe65f26
Hamza EL MAKRINI <elmakrinihamza1@gmail.com>
2021-01-14 13:25:33 +0100

    edit libraries 2

--- code/index.py
+++ code/index.py
@@ -1,11 +1,11 @@
 from PyQt5.QtWidgets import QApplication
 from controller import *
 import sys
-from pyqt5_material import apply_stylesheet
+from qt_material import apply_stylesheet
 
 def main():
     app = QApplication(sys.argv)
-    apply_stylesheet(app, theme='light_blue.xml',light_secondary=True)
+    apply_stylesheet(app, theme='light_blue.xml',invert_secondary=True)
     controller = Controller()
     controller.showLogin()
     app.exec_()
